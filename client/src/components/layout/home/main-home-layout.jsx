c6063defc6cad6809e68140d1dde2373a9bde1d2
PALALA\bthin <bthinh003@gmail.com>
2025-09-18 03:25:51 +0700

    no message

--- client/src/components/layout/home/main-home-layout.jsx
+++ client/src/components/layout/home/main-home-layout.jsx
@@ -5,14 +5,10 @@ import { Chatbot } from "@/bot/modern-chatbot"
 
 export const HomeLayout = () => {
     return (
-        <div className="relative min-h-screen flex flex-col bg-background">
+        <div>
             <GiftMeHeader />
-            <main className="flex-grow p-4 md:p-6">
-                <Outlet />
-            </main>
-
+            <Outlet />
             <Chatbot />
-
             <GiftMeFooter />
         </div>
     )
